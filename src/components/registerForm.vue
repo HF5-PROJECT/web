5629f2ad6f96393070a455cd8c46578fc577b0da
Anders <Anders164a@gmail.com>
2023-02-09 14:35:09 +0100

    #6 - Added changes according to feedback

--- src/components/registerForm.vue
+++ src/components/registerForm.vue
@@ -1,25 +1,25 @@
 <template>
-    <div class="mx-auto p-5 rounded-3xl w-min" style="background-color: #313030;">
+    <div class="mx-auto p-5 rounded-3xl w-min bg-primary-600">
         <div class="p-5">
             <h1>Register</h1>
             <p>Fill all fields</p>
             <div class="mt-12">
-                <form>
-                    <p v-if="parsedMessage" class="text-red-600 w-full mb-4 text-xs">{{ parsedMessage }}</p>
+                <form class="space-y-5">
                     <div class="input-div">
-                        <input class="w-full darkcolor lightbg mb-6 p-1.5 rounded-xl" type="text" placeholder="Name" v-model="name" />
+                        <BaseInput id="name" type="text" v-model="name" :error-text="nameErrorMessage" place-holder="Name"></BaseInput>
                     </div>
                     <div class="input-div">
-                        <input class="w-full darkcolor lightbg mb-6 p-1.5 rounded-xl" type="email" placeholder="E-mail" v-model="email" />
+                        <BaseInput id="email" type="email" v-model="email" :error-text="emailErrorMessage" place-holder="Email"></BaseInput>
                     </div>
                     <div class="input-div">
-                        <input class="w-full darkcolor lightbg mb-6 p-1.5 rounded-xl" type="password" placeholder="Password" v-model="password" />
+                        <BaseInput id="password" type="password" v-model="password" :error-text="passwordErrorMessage" place-holder="Password"></BaseInput>
                     </div>
                     <div class="input-div">
-                        <input class="w-full darkcolor lightbg mb-6 p-1.5 rounded-xl" type="password" placeholder="Confirm Password" v-model="confirmPassword" />
+                        <BaseInput id="passwordConfirm" type="password" v-model="confirmPassword" :error-text="passwordConfirmErrorMessage" place-holder="Confirm Password"></BaseInput>
                     </div>
-                    <div class="mt-6">
-                        <button type="submit" class="w-full darkbg rounded-xl p-2" @click.prevent="register">Register</button>
+                    <div>
+                    <p v-if="errorMessage" class="text-red-600 w-full mb-4 text-xs">{{ errorMessage }}</p>
+                        <button type="submit" class="w-full bg-primary-500 rounded-xl p-2" @click.prevent="register">Register</button>
                     </div>
                 </form>
             </div>
@@ -28,7 +28,11 @@
 </template>
 
 <script lang="ts">
+import BaseInput from "./baseInput.vue";
 export default {
+    components: {
+        BaseInput
+    },
     data() {
         return {
             name: "",
@@ -36,47 +40,25 @@ export default {
             password: "",
             confirmPassword: "",
             errorMessage: "",
-            validPassword: false,
-            passwordsMatch: false
-        }
-    },
-    props: {
-        endpoint: {
-            type: String,
-            required: true,
+            nameErrorMessage: "",
+            emailErrorMessage: "",
+            passwordErrorMessage: "",
+            passwordConfirmErrorMessage: ""
         }
     },
     computed: {
-        parsedMessage(): String {
-            return this.errorMessage;
-        }
-    },
-    watch: {
-        password() {
-            const length = this.password.length > 8;
-            const containsUppercase = /[A-Z]/.test(this.password);
-            const containsLowercase = /[a-z]/.test(this.password);
-            const containsNumber = /[0-9]/.test(this.password);
-            const containsSpecial = /[#?!@$%^&*-]/.test(this.password);
-            this.validPassword = length && containsUppercase && containsLowercase && containsNumber && containsSpecial;
-        },
-        confirmPassword() {
-            this.passwordsMatch = this.password === this.confirmPassword;
+        doesPasswordMatch() {
+            return this.password === this.confirmPassword;
         }
     },
     methods: {
         register(): void {
-            if (!this.validPassword) {
-                this.errorMessage = "Password is required to be minimum 8 characters long, with atleast one number, lowercase, uppercase and a special character";
-                return;
-            }
-
-            if (!this.passwordsMatch) {
+            if (!this.doesPasswordMatch) {
                 this.errorMessage = "Password fields should be identical";
                 return;
             }
 
-            fetch(this.endpoint, {
+            fetch("/api/auth/register", {
                 method: "POST",
                 headers: {
                 'Accept': 'application/json',
@@ -90,11 +72,11 @@ export default {
                 })
             })
             .then(async (response) => {
-                let json = await response.json();
-                if (json.statusCode !== undefined && json.statusCode !== 201) {
-                    this.errorMessage = json.message;
-                } else {
+                const json = await response.json();
+                if (json.statusCode === 201) {
                     window.location.href = "/";
+                } else{
+                    this.errorMessage = json.message;
                 }
             }).catch((response) => {
                 console.log(response);
@@ -105,20 +87,7 @@ export default {
     }
 </script>
 <style lang="scss">
-	.input-div {
-		width: 300px;
-	}
-	input::placeholder {
-		color: #56483E;
-		font-weight: 500;
-	}
-    .darkbg {
-        background-color: #56483E;
-    }
-    .lightbg {
-        background-color: #CFB39D;
-    }
-    .darkcolor {
-        color: #56483E;
+    .input-div {
+        width: 300px;
     }
 </style>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,10 +2,19 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
-		container: {
-			center: true,
-		}
+		extend: {
+			colors: {
+				'primary': {
+					100: "#fff",
+					200: "#cfb39d",
+					300: "#a68f7d",
+					400: "#7e6b5d",
+					500: "#56483e",
+					600: "#303030",
+					700: "#212121",
+				}
+			}
+		},
 	},
 	plugins: [],
 }
