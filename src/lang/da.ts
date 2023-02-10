54b18f82025343d67820271b5c210b71bb20e587
frederikpyt <frederikpyt@gmail.com>
2023-02-10 10:27:04 +0100

    #12 - Added i18n package to support multiple languages

---
+++ src/lang/da.ts
@@ -0,0 +1,14 @@
+export default {
+  language: "Sprog",
+  auth: {
+    register: "Registrer",
+    fillOutYourInfo: "Udfyld dine oplysninger",
+    name: "Navn",
+    email: "Email",
+    password: "Password",
+    confirmPassword: "Bekræft password",
+    alreadyHaveAnAccount: "Har du allerede en konto?",
+    login: "Login",
+    dontHaveAnAccount: "Har du ikke en konto?",
+  },
+};
