e785eab0835f38a7a76aea3a43f31674b40a0f3b
Kristian Binau <kristian@binau.me>
2023-03-06 09:54:14 +0100

    #23 - More admin panel

--- src/components/admin/hotel.vue
+++ src/components/admin/hotel.vue
@@ -8,6 +8,8 @@
                             <label for="name" class="block text-sm font-medium leading-6 text-gray-900">Name</label>
                             <input v-model="hotel.name" type="text" name="name" id="name" autocomplete="off"
                                 class="form-input mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6" />
+                            <p v-if="errorMessage.includes('name')" class="mt-2 text-sm text-red-500">{{
+                                computedErrorMessage }}</p>
                         </div>
 
                         <div>
@@ -17,19 +19,24 @@
                                 <textarea v-model="hotel.description" id="description" name="description" rows="3"
                                     class="form-textarea mt-1 block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:py-1.5 sm:text-sm sm:leading-6"></textarea>
                             </div>
+                            <p v-if="errorMessage.includes('description')" class="mt-2 text-sm text-red-500">{{
+                                computedErrorMessage }}</p>
                         </div>
 
                         <div class="col-span-6">
                             <label for="street-address"
                                 class="block text-sm font-medium leading-6 text-gray-900">Address</label>
-                            <input v-model="hotel.address" type="text" name="street-address" id="street-address" autocomplete="street-address"
+                            <input v-model="hotel.address" type="text" name="street-address" id="street-address"
+                                autocomplete="street-address"
                                 class="form-input mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6" />
+                            <p v-if="errorMessage.includes('address')" class="mt-2 text-sm text-red-500">{{
+                                computedErrorMessage }}</p>
                         </div>
                     </div>
                     <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
-                        <button type="submit"
-                            class="inline-flex mr-4 justify-center rounded-md bg-red-600 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-500">Save</button>
-                        <button @click="update()"
+                        <button @click="deleteHotel()"
+                            class="inline-flex mr-4 justify-center rounded-md bg-red-600 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-500">Delete</button>
+                        <button @click="updateOrCreateHotel()"
                             class="inline-flex justify-center rounded-md bg-blue-600 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500">Save</button>
                     </div>
                 </div>
@@ -40,6 +47,12 @@
 
 <script lang="ts">
 export default {
+    data() {
+        return {
+            errorMessage: "",
+        };
+    },
+
     props: {
         hotel: {
             type: Object,
@@ -47,18 +60,69 @@ export default {
         },
     },
 
+    computed: {
+        computedErrorMessage() {
+            let errorMessage = this.errorMessage;
+
+            errorMessage = errorMessage.replace('body/', '');
+
+            return errorMessage.charAt(0).toUpperCase() + errorMessage.slice(1);;
+        },
+    },
+
     methods: {
-        update() {
-            fetch(import.meta.env.PUBLIC_API_URL + "/hotel/" + this.hotel.id, {
-                method: "PUT",
-                headers: {
-                    Accept: "application/json",
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(this.hotel)
-            }).then((response) => {
+        async updateOrCreateHotel() {
+            let response;
+            if (this.hotel.id === undefined) {
+                response = await fetch(import.meta.env.PUBLIC_API_URL + "/hotel/", {
+                    method: "POST",
+                    headers: {
+                        Accept: "application/json",
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(this.hotel)
+                })
+
+                this.$emit('create')
+            } else {
+                response = await fetch(import.meta.env.PUBLIC_API_URL + "/hotel/" + this.hotel.id, {
+                    method: "PUT",
+                    headers: {
+                        Accept: "application/json",
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(this.hotel)
+                });
+
+                this.$emit('update')
+            };
+
+            const json = await response.json();
+
+            if (response.status === 400) {
+                this.errorMessage = json.message;
+            }
+        },
+
+        async deleteHotel() {
+            if (this.hotel.id !== undefined) {
+                let response = await fetch(import.meta.env.PUBLIC_API_URL + "/hotel/" + this.hotel.id, {
+                    method: "DELETE",
+                    headers: {
+                        Accept: "application/json",
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(this.hotel)
+                })
+
                 console.log(response)
-            });
+
+                if (response.status !== 204) {
+                    return;
+                }
+            }
+
+            this.$emit('delete')
         },
     },
 };
---
+++ src/components/admin/hotels.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="flex flex-col flex-grow rounded mt-4">
+        <button v-on:click="addHotel()">MORE!</button>
+        <Hotel v-for="(hotel, index) in hotels" :hotel="hotel" @delete="deleteHotel(index)"></Hotel>
+    </div>
+</template>
+
+<script lang="ts">
+import Hotel from './hotel.vue';
+
+export default {
+    components: {
+        Hotel,
+    },
+
+    data() {
+        return {
+            hotels: [] as Array<Object>,
+        };
+    },
+
+    props: {
+        hotelsData: {
+            type: Array<Object>,
+            required: true,
+        },
+    },
+
+    mounted() {
+        this.hotels = this.hotelsData;
+    },
+
+    methods: {
+        addHotel() {
+            this.hotels.unshift({
+                name: '',
+                description: '',
+                address: '',
+            })
+        },
+
+        deleteHotel(index: number) {
+            this.hotels.splice(index, 1);
+        }
+    },
+};
+</script>
