281cc001d61f0a246c0870f4a1999dd6c1e7ea4e
Kristian Binau <kristian@binau.me>
2023-03-07 09:29:09 +0100

    #23 - Fix error handling

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -23,7 +23,7 @@ module.exports = {
   plugins: [
     require('@tailwindcss/line-clamp'),
     require("@tailwindcss/forms")({
-      strategy: 'class', // only generate classes
+      strategy: 'class',
     }),
   ],
 };
